3f6bbf6b4a59c3f9e101a612e249395a8230e69a
rupom <furqanrupom978@gmail.com>
2023-03-27 16:44:59 +0600

    added seleted data by using usetate hooks

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,25 +3,36 @@ import Product from './Product';
 
 const Content = () => {
     const [products,setProducts] = useState([]);
+    const [cart,setCart] = useState([]);
     useEffect(()=>{
         const loadProducts = async() =>{
             const res = await fetch('products.json')
             const data = await res.json()
             setProducts(data)
-
         }
         loadProducts();
     },[])
+    
+    const handleProduct = (product)=>{
+        const newCart = [...cart,product]
+        console.log(newCart);
+        setCart(newCart);
+    }
     return (
         <main className="my-20 ">
             <section className="grid grid-cols-[4fr,1fr]">
                 <div className="grid grid-cols-3 gap-x-10 gap-y-20">
                     {
-                        products.map(product => <Product key={product.id} product={product} />)
+                        products.map(product => <Product
+                            key={product.id}
+                            product={product}
+                            handleProduct={handleProduct}
+                            />)
                     }
                 </div>
                 <div>
                     <h1>orders summary</h1>
+                    <p>items Selected {cart.length}</p>
                 </div>
             </section>
         </main>
--- src/components/Content/Product.jsx
+++ src/components/Content/Product.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
   const { name, price, seller, category, img ,ratings} = props.product;
+  const getHandleProduct = props.handleProduct
   return (
     <>
-      <div className=" px-2 pt-2 pb-8 border border-slate-400 rounded-md">
+      <div className=" px-2 pt-2 pb-8 border border-slate-400 rounded-md relative">
         <img className="rounded-md" src={img} alt="" />
         <div className="py-5">
           <h2 className="text-xl font-semibold">{name}</h2>
           <p>
             price : <span className="font-semibold">{price}</span>
           </p>
-          <div className="pt-5">
+          <div className="pt-5 pb-5">
                 <p>Manufacturer : {seller}</p>
                 <p>ratings : {ratings} star</p>
           </div>
         </div>
+          <button onClick={()=> getHandleProduct(props.product)} className="px-7 py-3  absolute left-0 bottom-0 w-full bg-amber-400/25 font-semibold hover:bg-amber-400/50 transition-all ">
+            Add to Cart
+             <span className="ml-8 text-xl"><FontAwesomeIcon icon={faShoppingCart} /></span>
+            </button>
 
       </div>
     </>
